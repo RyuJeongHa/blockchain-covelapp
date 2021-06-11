f458b5fdb511bfd2347dd3e4c489c8ebf29072b5
RyuJeongHa <fbwjdgk00@bu.ac.kr>
2021-06-11 11:09:07 +0900

    수정한 내용 2021-06-11 11:08

--- app/src/main/java/com/example/covel/Novel_upload.java
+++ app/src/main/java/com/example/covel/Novel_upload.java
@@ -1,15 +1,26 @@
 package com.example.covel;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.OpenableColumns;
 import android.text.Spannable;
 import android.text.Spanned;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,8 +29,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -33,15 +46,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+import lombok.val;
+
+import static androidx.constraintlayout.motion.utils.Oscillator.TAG;
+import static com.example.covel.Novel_registration.getFullPathFromUri;
+
 public class Novel_upload extends AppCompatActivity {
+    private static final int REQUEST_CODE = 0;
+    private static final int SELECT_FILE = 0;
+    private static final int FILE_SELECT_OK = 0;
     ImageButton imgBackBtn5;
     Button btnUpload;
-    TextView write_novel,file_name,upload_title, findNovelTextView, attached_file; //내 소설 찾기, 첨부파일
+    TextView write_novel,file_name,upload_title, findNovelTextView, attached_file;
     ArrayList<String> titleList;
+    private static final int READ_REQUEST_CODE = 123;
 
 
     @Override
@@ -147,39 +174,104 @@ public class Novel_upload extends AppCompatActivity {
         //findNovelTextView - upload_title(EditText)에서 친 내용을 내 소설 찾기 텍스트를 누르면
         // DB에 등록된 내 소설(제목과 설명과 표지가 들어간)을 찾아주기
 
+
         attached_file.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-                i.setType("application/*");
-                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(i.createChooser(i,"OPEN"));
 
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                Uri uri = Uri.parse(Environment.getDownloadCacheDirectory().getPath()+"/Download/");
-                intent.setDataAndType(uri,"application/*");
-                startActivity(intent.createChooser(intent,"OPEN"));
-
-                Intent it = new Intent(Intent.ACTION_GET_CONTENT);
-                it.setType("text/plain");
-                Uri uri2=Uri.parse(Environment.getDownloadCacheDirectory().getPath()+"text/");
-                it.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(it.createChooser(it,"OPEN"));
-                file_name.setText(uri2.getLastPathSegment());
-
-
-
-
-
+                 Intent it = new Intent(Intent.ACTION_GET_CONTENT);
+                 it.setType("text/plain");
+                 //Uri uri2=Uri.parse(Environment.getDownloadCacheDirectory().getPath()+"text/");
+                 //File file = new File(Environment.getDownloadCacheDirectory().getPath()+"text/");
+                 //String filename = file.getName();
+                 //Log.d("TEST",filename);
+                 //String strFile = it.toString();
+                 it.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                 //startActivity(it.createChooser(it,"OPEN"));
+                 //file_name.setText(strFile);
+                 startActivityForResult(it.createChooser(it,"Select a file"),
+                         READ_REQUEST_CODE);
 
             }
+
         });//attached_file
         // 클릭하면 파일탐색기가 나와서 파일 첨부가 가능
         // 선택한 파일이 file_name칸에 보여져야 함
 
-    }//onCreate
+    } //onCreate
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == READ_REQUEST_CODE && resultCode == RESULT_OK){
+            Uri selectedfile = data.getData();
+            //경로 정보: selectedfile.getPath();
+            //전체 URI 정보: selectedfile.to(String);
+            Toast.makeText(getApplicationContext(),getFileNameFromUri(selectedfile),
+                    Toast.LENGTH_LONG).show();
+            //Log d(TAG,"SELECTED:"+selectedfile.toString());
+        }
+    }//onActivityResult
+
+    //URI에서 파일명 얻기
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String getFileNameFromUri(Uri uri){
+        String fileName = "";
+        Cursor cursor = getContentResolver().query(uri,null,null,null);
+        if(cursor != null && cursor.moveToFirst()){
+            fileName=cursor.getString(
+                cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+            Log.i(TAG,"Display Name: "+fileName);
+            }
+        cursor.close();
+        return fileName;
+        }
 
 
+    /*
+    public void mOnFileRead(View v){
+        String read = ReadTextFile(filePath);
+        write_novel.setText(read);
+    }//mOnFileRead
 
+    //경로의 텍스트 파일 읽기
+    public String ReadTextFile(String path){
+        StringBuffer strBuffer = new StringBuffer();
+        try{
+            InputStream is = new FileInputStream(path);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String line="";
+            while((line=reader.readLine())!=null){
+                strBuffer.append(line+"\n");
+            }
 
+            reader.close();
+            is.close();
+        }catch (IOException e){
+            e.printStackTrace();
+            return "";
+        }
+        return strBuffer.toString();
+    }*/
+
+    /*
+    private String getStringAssetFile(Activity activity)throws Exception{
+        AssetManager as = activity.getAssets();
+        InputStream is = as.open("text/plain");
+        String text = converStreamToString(is);
+        is.close();
+        return text;
+    }
+
+    private String converStreamToString(InputStream is)throws IOException{
+        ByteArrayOutputStream bs = new ByteArrayOutputStream();
+        int i = is.read();
+        while (i != -1){
+            bs.write(i);
+            i=is.read();
+        }
+        return bs.toString();
+    }*/
 }//Novel_upload
+
